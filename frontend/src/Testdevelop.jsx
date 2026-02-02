c3c51d2c03aea9068f061a70bdc98b48455f4d4f
Adjie <Adjie@gmail.com>
2026-02-02 12:25:07 +0800

    adjie local graph

---
+++ frontend/src/Testdevelop.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Testadjie from "./FrontendScript/Adjie/Testadjie";
+function Testdevelop() {
+ 
+  return (
+    <div className="container_dev" style={{ backgroundColor: "#0f1fcb" }}>
+      <Testadjie />
+    </div>
+  );
+}
+
+export default Testdevelop;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,21 +3,14 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import { Toaster } from "react-hot-toast";
-import Motherparent from "./Motherparent.jsx";
+import Testdevelop from "./Testdevelop";
 
 
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <Toaster
-        position="top-right"
-        toastOptions={{
-          style: { zIndex: 99999 }
-        }}
-      />
-      <Motherparent />
+      <Testdevelop />
     </BrowserRouter>
   </StrictMode>
 );
