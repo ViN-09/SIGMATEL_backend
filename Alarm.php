747dbe1a80ffb99d16117a7c7ee18eda331dcf9a
Andikaamm <andika.maulana001@gmail.com>
2026-02-05 21:19:09 +0800

    test dika

--- Alarm.php
+++ Alarm.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
-
+//tes dika
 class Alarm extends Controller
 {
     protected $connection = 'mysql2';
