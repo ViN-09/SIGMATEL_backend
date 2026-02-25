7da10bdde435460605ca6decaa276215f6324718
Zein <email@domain.com>
2026-02-25 11:03:55 +0800

    api example

--- Routes/api.example.php
+++ Routes/api.example.php
@@ -1,24 +1,15 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Support\Facades\Route;
-use Carbon\Carbon;
-
-// Controler Trial
-use App\Http\Controllers\ttc_paniki_controllers\data_potensi as DataPotensi2Paniki;
 use App\Http\Controllers\ttc_paniki_controllers\checklist as CheckListPaniki2;
+use App\Http\Controllers\ttc_paniki_controllers\data_potensi as DataPotensi2Paniki;
+use App\Http\Controllers\ttc_paniki_controllers\ProfilesController;
 use App\Http\Controllers\ttc_paniki_controllers\summary_pue as SummaryPuePaniki;
-use App\Http\Controllers\ttc_paniki_controllers\VisitorsController; // Import VisitorsController
-use App\Http\Controllers\ttc_paniki_controllers\ProfilesController as PanikiProfilesController;
 use App\Http\Controllers\ttc_paniki_controllers\UserController;
+use App\Http\Controllers\ttc_paniki_controllers\VisitorsController;
+use Illuminate\Support\Facades\Route;
 
-
-
-/////////////////////////////////////////////////////////////////////////////Paniki/////////////////////////////////////////////////////////////////////////////////////////
 Route::prefix('ttc_paniki')->group(function () {
-    
+
     Route::prefix('data_potensi')->group(function () {
         Route::get('/fullDapot', [DataPotensi2Paniki::class, 'getAllDataPotensi']);
     });
@@ -26,28 +17,24 @@ Route::prefix('ttc_paniki')->group(function () {
     Route::prefix('checklist2')->group(function () {
         Route::get('/dialyActivityList/{ym}', [CheckListPaniki2::class, 'dialyActivityListByMonth']);
         Route::get('/pullreport/{id}/{type}', [CheckListPaniki2::class, 'pullReport']);
-        Route::get('dialyActivityList', [CheckListPaniki2::class, 'dialyActivityList']);
+        Route::get('/dialyActivityList', [CheckListPaniki2::class, 'dialyActivityList']);
     });
 
     Route::prefix('summary_pue')->group(function () {
         Route::get('/data_report/{type}/{startDate?}/{endDate?}', [SummaryPuePaniki::class, 'tableReportList']);
     });
 
-    
-    Route::prefix('visitors')->group(function () {
-        
-        Route::get('/', [VisitorsController::class, 'getVisitors']);
+    // Visitors
+    Route::post('/visitor/registry', [VisitorsController::class, 'registvisitor']);
+    Route::get('/visitor', [VisitorsController::class, 'index']);
+    Route::get('/visitor/waiting', [VisitorsController::class, 'waiting']); 
+    Route::post('/visitor/{id}/update-status', [VisitorsController::class, 'updateStatus']);
 
-        
-        Route::post('/add', [VisitorsController::class, 'addVisitor']);
+    Route::prefix('user')->group(function () {
+        Route::get('/{id}', [UserController::class, 'show']);
     });
 
-    Route::get('profiles', [PanikiProfilesController::class, 'profiles']);
-});
+        Route::get('/stafflist/{jabatan}', [UserController::class, 'staffList']);
 
-Route::prefix('user')->group(function () {
-
-    Route::get('{id}', [UserController::class, 'show']);
-
-    Route::get('stafflist/{site}/{jabatan}', [UserController::class, 'staffList']);
+        Route::get('/profiles', [ProfilesController::class, 'profiles']);
 });
